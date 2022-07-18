4770fccf6d6a7713ee45785d6586779c86671bc2
ChungGold <74089047+ChungGold@users.noreply.github.com>
2022-07-18 09:42:28 -0700

    Add files via upload
    Added player health, monster turn, and json card objects

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
+import playerCards from './playerCards.json' assert {type: 'json'};
+
 let mHealth = 100;
+let pHealth = 100;
 
-console.log(mHealth);
+console.log(playerCards.playerDeck[0].cardEffect.toString);
 
-const card = document.getElementsByClassName('card');
+const pCard = document.getElementsByClassName('p-card');
 
-console.log(card);
+let mTurn = () => {
+    if (mHealth <= 0) {
+        return;
+    }
+    pHealth = pHealth - 8;
+    console.log('player hp ' + pHealth);
+    if (pHealth <= 0) {
+        alert('you have been slain!');
+    }
+}
 
-card[0].addEventListener("click", function() {
-    mHealth = mHealth-5;
-    console.log(mHealth);
+pCard[0].addEventListener("click", function() {
+    mHealth = mHealth-10;
+    console.log('monster hp ' + mHealth);
+    if (mHealth <= 0) {
+        alert('monster slain!');
+    }
+    mTurn();
 });
